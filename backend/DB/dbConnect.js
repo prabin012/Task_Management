9bb0f4a4a78ef7738742ecddaba83db11a3fca83
Prabin Kumar Mahato <prabinmahato414@gmail.com>
2023-10-07 22:03:39 +0545

    updater

--- backend/DB/dbConnect.js
+++ backend/DB/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const database =()=>{
-    mongoose.connect('mongodb://127.0.0.1:27017/Tasks')
+    mongoose.connect('mongodb://localhost:27017/Tasks')
     .then(()=>{
         console.log("database connected")
     }).catch((e)=>{
